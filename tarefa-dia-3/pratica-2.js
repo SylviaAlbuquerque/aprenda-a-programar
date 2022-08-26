efe59fcd54f09fe6e5de40ec63c94985822126ba
Sylvia Albuquerque <sylvia_albuquerque@hotmail.com>
2022-08-26 16:21:11 -0300

    Subindo pratica-2 tabuada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+/* Pratica 2: Fazendo a tabuada */
+
+let multiplicador = 10
+let n = 10
+let i = 1
+
+while (i <= n) {
+   console.log (`${multiplicador} x ${i} = ${multiplicador * 1}`)
+    i++
+}
